e28e3e5d4ba8502af6e081e0dd02142b1744b6b4
Zeeta:) <113794999+z3t444@users.noreply.github.com>
2024-03-16 03:02:23 +0700

    <ui>(Update Section)

--- src/content/section/SprintFork.jsx
+++ src/content/section/SprintFork.jsx
@@ -1,18 +1,20 @@
+import Asset from "../components/SprintForkActifity/asset/unnamed.png";
+
 function SprintFork() {
   return (
     <>
-      <div className="border-2xl bg-gradient-to-r mt-5 rounded-xl from-[#ffb132] to-[#fb8c9d]">
-        <div className="flex items-center justify-center">
-            {/* desk */}
+      <div className="border-2xl py-12 bg-gradient-to-r mt-5 rounded-xl from-[#ffb132] to-[#fb8c9d]">
+        <div className="flex items-center gap-3 justify-center">
+          {/* desk */}
           <div className="grid">
-            <h3 className="text-gray-200">Forking Activity</h3>
-            <p className="text-gray-300">anytime Project</p>
+            <h3 className="text-gray-200 font-medium">Forking Activity</h3>
+            <p className="text-gray-300 mt-2 text-sm">anytime Project</p>
           </div>
           {/* asset image picture */}
-          <div className="flex items-center justify-center">
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <div className="flex items-center gap-3 justify-center">
+            <img src={Asset} className="rounded-full" width={50} alt="" />
+            <img src={Asset} className="rounded-full" width={50} alt="" />
+            <img src={Asset} className="rounded-full" width={50} alt="" />
           </div>
         </div>
       </div>
