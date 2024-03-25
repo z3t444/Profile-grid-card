6b35e93b4ab9cba6f5a600e4bddd81b1efd50397
Zeeta:) <113794999+z3t444@users.noreply.github.com>
2024-03-26 03:24:44 +0700

    test

--- src/content/section/Framework.jsx
+++ src/content/section/Framework.jsx
@@ -1,32 +1,17 @@
-import Css from "../components/Framework/asset/Css.svg";
-import Html from "../components/Framework/asset/Html.svg";
-import Javascript from "../components/Framework/asset/Javascript.svg";
-import Nest from "../components/Framework/asset/Nest.svg";
-import Nuxt from "../components/Framework/asset/Nuxt.svg";
-import Pug from "../components/Framework/asset/Pug.svg";
-import React from "../components/Framework/asset/React.svg";
-import Sass from "../components/Framework/asset/Sass.svg";
-import Svelte from "../components/Framework/asset/Svelte.svg";
-import Tailwindcss from "../components/Framework/asset/Tailwindcss.svg";
-import Vue from "../components/Framework/asset/Vue.svg";
-
 function Framework() {
   return (
     <>
-      <div className="max-w-sm rounded-2xl px-6 py-8 mt-5 bg-[#171717]">
+      <div className="max-w-sm px-5 mt-5 bg-[#171717] rounded-2xl py-9">
         <marquee direction="">
           <div className="flex items-center gap-10">
-            <img src={Css} alt="" />
-            <img src={Html} alt="" />
-            <img src={Javascript} alt="" />
-            <img src={Nest} alt="" />
-            <img src={Nuxt} alt="" />
-            <img src={Pug} alt="" />
-            <img src={React} alt="" />
-            <img src={Sass} alt="" />
-            <img src={Svelte} alt="" />
-            <img src={Tailwindcss} alt="" />
-            <img src={Vue} alt="" />
+            <img src={CSS} alt="" />
+            <img src={HTML} alt="" />
+            <img src={JS} alt="" />
+            <img src={NEST} alt="" />
+            <img src={NUXT} alt="" />
+            <img src={PUG} alt="" />
+            <img src={REACT} alt="" />
+            <img src={SASS} alt="" />
           </div>
         </marquee>
       </div>
@@ -35,3 +20,12 @@ function Framework() {
 }
 
 export default Framework;
+
+import CSS from "../components/Framework/asset/Css.svg";
+import HTML from "../components/Framework/asset/Html.svg";
+import JS from "../components/Framework/asset/Javascript.svg";
+import NEST from "../components/Framework/asset/Nest.svg";
+import NUXT from "../components/Framework/asset/Nuxt.svg";
+import PUG from "../components/Framework/asset/Pug.svg";
+import REACT from "../components/Framework/asset/React.svg";
+import SASS from "../components/Framework/asset/Sass.svg";
